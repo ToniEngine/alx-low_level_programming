120791edb8c96a7bb5c29a234a14ca931bb63fb0
Anthony Obot <antoineorbot@gmail.com>
2022-10-21 02:11:52 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing the functions
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+
